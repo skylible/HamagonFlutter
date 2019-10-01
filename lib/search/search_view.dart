093f9921ba9bb0dc357b106b3191e5f7818702c1
skylible <sourcewidi@gmail.com>
2019-10-01 21:21:29 +0700

    Searchview progress

--- lib/search/search_view.dart
+++ lib/search/search_view.dart
@@ -5,80 +5,121 @@ import 'package:hamagon/image_search/main.dart';
 class SearchView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(elevation: 0,
-        bottomOpacity: 0,
-        centerTitle: true,
-        title: Text("HAMAGON"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(12),
-              color: Colors.green,
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(),
-                  ),
-                  Container(
-                    child: TextField(
-                      decoration: InputDecoration(
-                          fillColor: Colors.white,
-                          prefixIcon: Icon(Icons.search),
-                          border: OutlineInputBorder(
-                            borderRadius: const BorderRadius.all(
-                              const Radius.circular(40.0),
-                            ),
-                          ),
-                          hintText: 'Enter a search term'),
-                    ),
-                  ),
-                ],
-              ),
+    return Stack(
+      children: <Widget>[
+        Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/farmland.jpg"),
+              fit: BoxFit.cover,
             ),
           ),
-          Flexible(
-              flex: 1,
-              child: GridView.count(
-                primary: false,
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 2,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => MainImage()),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      child: const Icon(Icons.camera_alt),
-                      color: Colors.teal[100],
-                    ),
+        ),
+        Column(
+          children: <Widget>[
+            Flexible(
+              flex: 10,
+              child: Container(),
+            ),
+            Flexible(
+              flex: 7,
+              child: Container(
+                color: Colors.grey[100],
+              ),
+            ),
+          ],
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            bottomOpacity: 0,
+            centerTitle: true,
+            title: Text("HAMAGON"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(),
+                      ),
+                      Container(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              prefixIcon: Icon(Icons.search),
+                              border: OutlineInputBorder(
+                                borderRadius: const BorderRadius.all(
+                                  const Radius.circular(40.0),
+                                ),
+                              ),
+                              hintText: 'Enter a search term'),
+                        ),
+                      ),
+                    ],
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => DetailMain()),
-                      );
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.all(8),
-                      child: const Icon(Icons.list),
-                      color: Colors.teal[100],
-                    ),
-                  ),
-                ],
-              ))
-        ],
-      ),
+                ),
+              ),
+              Flexible(
+                  flex: 1,
+                  child: GridView.count(
+                    primary: false,
+                    padding: const EdgeInsets.all(20),
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    crossAxisCount: 2,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MainImage()),
+                          );
+                        },
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          elevation: 3,
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            child: const Icon(Icons.camera_alt),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => DetailMain()),
+                          );
+                        },
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          elevation: 3,
+                          child: Container(
+                            padding: const EdgeInsets.all(8),
+                            child: const Icon(Icons.list),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
