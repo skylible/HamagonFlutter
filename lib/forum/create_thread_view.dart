a5bf40702148b33cad03935d1f5e30b5d68916fe
skylible <sourcewidi@gmail.com>
2019-10-04 18:50:56 +0700

    last commit before combining: beutifying profile and create thread page

--- lib/forum/create_thread_view.dart
+++ lib/forum/create_thread_view.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:hamagon/forum/forum_repo.dart';
+import 'package:hamagon/model/auth_service.dart';
+import 'package:intl/intl.dart';
 
 class CreateThreadView extends StatefulWidget {
   final String topic;
@@ -14,61 +17,135 @@ class _CreateThreadViewState extends State<CreateThreadView> {
   final _formKey = GlobalKey<FormState>();
   final ForumRepository forumRepo = ForumRepository();
   TextEditingController contentController = TextEditingController();
+  AuthService authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Buat pertanyaan"),
+        title: Text("Buat pertanyaan baru"),
       ),
-      body: Builder(
-        builder: (context) => Flex(
-          direction: Axis.vertical,
-          children: <Widget>[
-            Form(
-              key: _formKey,
-              child: TextFormField(
-                controller: contentController,
-                // The validator receives the text that the user has entered.
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Teks masih kosong';
-                  }
-                  if (value.length > 280) {
-                    return 'Teks melebihi batas 280 karakter';
-                  }
-                  return null;
-                },
+      body: FutureBuilder(
+          future: _getUser(),
+          builder:
+              (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return Card(
+              child: Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 4, 0, 16),
+                      child: Flex(
+                        direction: Axis.horizontal,
+                        children: <Widget>[
+                          Container(
+                              child: CircleAvatar(
+                            backgroundImage: snapshot.data.photoUrl == null
+                                ? AssetImage(
+                                    'assets/images/avatar-placeholder.png')
+                                : NetworkImage(snapshot.data.photoUrl),
+                            radius: 24,
+                          )),
+                          Container(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Padding(
+                                  padding: EdgeInsets.fromLTRB(6, 0, 0, 4),
+                                  child: Text(
+                                    snapshot.data.displayName,
+                                    textAlign: TextAlign.left,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                // Padding(
+                                //   padding: EdgeInsets.fromLTRB(6, 0, 0, 4),
+                                //   child: Text(
+                                //     DateFormat('E, d MMM  h:m')
+                                //         .format(ds['created_at'].toDate()),
+                                //     textAlign: TextAlign.left,
+                                //     style:
+                                //         TextStyle(fontWeight: FontWeight.w300),
+                                //   ),
+                                // )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Flex(
+                      direction: Axis.vertical,
+                      children: <Widget>[
+                        Form(
+                          key: _formKey,
+                          child: TextFormField(
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                            controller: contentController,
+                            // The validator receives the text that the user has entered.
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Teks masih kosong';
+                              }
+                              if (value.length > 280) {
+                                return 'Teks melebihi batas 280 karakter';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            onPressed: () {
+                              // Validate returns true if the form is valid, otherwise false.
+                              if (_formKey.currentState.validate()) {
+                                // If the form is valid, display a snackbar. In the real world,
+                                // you'd often call a server or save the information in a database.
+
+                                Scaffold.of(context).showSnackBar(SnackBar(
+                                    content: Text('Sedang diproeses...')));
+
+                                print("create Thread called");
+                                forumRepo
+                                    .createThread(
+                                        topic: widget.topic,
+                                        content: contentController.text)
+                                    .whenComplete(
+                                  () {
+                                    Scaffold.of(context).showSnackBar(SnackBar(
+                                        content: Text(
+                                            'Membuat postingan berhasil!')));
+                                    Navigator.pop(context);
+                                  },
+                                );
+                              }
+                            },
+                            child: Text('Kirim'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-            RaisedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, otherwise false.
-                if (_formKey.currentState.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Sedang diproeses...')));
-
-                  print("create Thread called");
-                  forumRepo
-                      .createThread(
-                          topic: widget.topic, content: contentController.text)
-                      .whenComplete(
-                    () {
-                      Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('Membuat postingan berhasil!')));
-                      Navigator.pop(context);
-                    },
-                  );
-                }
-              },
-              child: Text('Kirim'),
-            )
-          ],
-        ),
-      ),
+            );
+          }),
     );
+  }
+
+  Future<FirebaseUser> _getUser() {
+    return authService.getCurrentUser();
   }
 }
--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -11,177 +11,181 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: authService.getCurrentUser(),
-      builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) => Stack(
-        children: <Widget>[
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.green,
-            ),
-          ),
-          Column(
+        future: authService.getCurrentUser(),
+        builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: RefreshProgressIndicator(),
+            );
+          }
+          return Stack(
             children: <Widget>[
-              Flexible(
-                flex: 7,
-                child: Container(),
-              ),
-              Flexible(
-                flex: 10,
-                child: Container(
-                  color: Colors.grey[100],
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.green,
                 ),
               ),
-            ],
-          ),
-          Scaffold(
-            backgroundColor: Colors.transparent,
-            appBar: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              bottomOpacity: 0,
-              centerTitle: true,
-              title: Text("Profil"),
-            ),
-            body: Container(
-              padding: EdgeInsets.fromLTRB(6, 0, 6, 0),
-              height: double.infinity,
-              width: double.infinity,
-              child: Column(
+              Column(
                 children: <Widget>[
                   Flexible(
-                    flex: 4,
+                    flex: 7,
                     child: Container(),
                   ),
                   Flexible(
-                    flex: 8,
+                    flex: 10,
                     child: Container(
-                      width: double.infinity,
-                      height: double.infinity,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16)),
-                        elevation: 3,
-                        child: Column(
-                          children: <Widget>[
-                            Flexible(
-                              flex: 2,
-                              child: Container(),
-                            ),
-                            Flexible(
-                              flex: 1,
-                              child: Container(
-                                padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
-                                alignment: Alignment.center,
-                                child: Text(
-                                  snapshot.data.displayName,
-                                  style: TextStyle(fontSize: 24),
+                      color: Colors.grey[100],
+                    ),
+                  ),
+                ],
+              ),
+              Scaffold(
+                backgroundColor: Colors.transparent,
+                appBar: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  bottomOpacity: 0,
+                  centerTitle: true,
+                  title: Text("Profil"),
+                ),
+                body: Container(
+                  padding: EdgeInsets.fromLTRB(6, 0, 6, 0),
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: Column(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 4,
+                        child: Container(),
+                      ),
+                      Flexible(
+                        flex: 8,
+                        child: Container(
+                          width: double.infinity,
+                          height: double.infinity,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16)),
+                            elevation: 3,
+                            child: Column(
+                              children: <Widget>[
+                                Flexible(
+                                  flex: 2,
+                                  child: Container(),
                                 ),
-                              ),
+                                Flexible(
+                                  flex: 1,
+                                  child: Container(
+                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      snapshot.data.displayName,
+                                      style: TextStyle(fontSize: 24),
+                                    ),
+                                  ),
+                                ),
+                                Flexible(
+                                  flex: 1,
+                                  child: Container(
+                                    child: Text("petani jagung | Malang"),
+                                  ),
+                                ),
+                              ],
                             ),
-                            Flexible(
-                              flex: 1,
-                              child: Container(
-                                child: Text("petani jagung | Malang"),
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 3,
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(0, 12, 0, 8),
-                      height: double.infinity,
-                      width: double.infinity,
-                      child: RaisedButton(
-                        onPressed: () {},
-                        elevation: 3,
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Icon(Icons.edit),
+                      Flexible(
+                        flex: 3,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(0, 12, 0, 8),
+                          height: double.infinity,
+                          width: double.infinity,
+                          child: RaisedButton(
+                            onPressed: () {},
+                            elevation: 3,
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
                             ),
-                            Container(
-                              child: Text("Edit Profil"),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(Icons.edit),
+                                ),
+                                Container(
+                                  child: Text("Edit Profil"),
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 3,
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(0, 8, 0, 12),
-                      height: double.infinity,
-                      width: double.infinity,
-                      child: RaisedButton(
-                        onPressed: () {
-                          authService.signOutGoogle();
-                        },
-                        elevation: 3,
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Container(
-                              child: Icon(Icons.exit_to_app),
-                            ),
-                            Container(
-                              child: Text("Keluar"),
-                            ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
+                      Flexible(
+                        flex: 3,
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(0, 8, 0, 12),
+                          height: double.infinity,
+                          width: double.infinity,
+                          child: RaisedButton(
+                            onPressed: () {
+                              authService.signOutGoogle();
+                            },
+                            elevation: 3,
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(Icons.exit_to_app),
+                                ),
+                                Container(
+                                  child: Text("Keluar"),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(),
+                      ),
+                    ],
                   ),
+                ),
+              ),
+              Column(
+                children: <Widget>[
                   Flexible(
                     flex: 1,
                     child: Container(),
                   ),
-                ],
-              ),
-            ),
-          ),
-          Column(
-            children: <Widget>[
-              Flexible(
-                flex: 1,
-                child: Container(),
-              ),
-              Flexible(
-                flex: 3,
-                child: 
-                Container(
-                  padding: EdgeInsets.fromLTRB(92, 12, 92, 12),
-                  alignment: Alignment.center,
-                  child: 
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(snapshot.data.photoUrl),
-                    maxRadius: 80,
+                  Flexible(
+                    flex: 3,
+                    child: Container(
+                      padding: EdgeInsets.fromLTRB(92, 12, 92, 12),
+                      alignment: Alignment.center,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(snapshot.data.photoUrl),
+                        maxRadius: 80,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Flexible(
-                flex: 4,
-                child: Container(),
-              ),
+                  Flexible(
+                    flex: 4,
+                    child: Container(),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    );
+          );
+        });
   }
 }
